762bd64e72e12669e13dc9822cbbb882028a9afc
Zahaawii <zahaawii@gmail.com>
2025-11-19 10:55:37 +0100

    initialize project

---
+++ src/main/java/com/example/kromannreumert/KromannReumertApplication.java
@@ -0,0 +1,13 @@
+package com.example.kromannreumert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KromannReumertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KromannReumertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kromannreumert/KromannReumertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kromannreumert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KromannReumertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
